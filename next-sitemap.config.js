8a213e0a1c97a82606b5c096a5acc06c8b13dc11
RaFaIXI <129433000+RaFaIXI@users.noreply.github.com>
2025-05-04 14:30:42 +0200

    feat: add next-sitemap configuration and generate sitemap.xml

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://rematch-cookbook.vercel.app',
+    generateRobotsTxt: true,
+    outDir: './public', // This tells it to generate in the public folder
+  };
+  
---
+++ next-sitemap.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://rematch-cookbook.vercel.app',
+    generateRobotsTxt: true,
+  };
+  
