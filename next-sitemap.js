daa2b850743c22b214c7e02202145b2f968ddadd
RaFaIXI <129433000+RaFaIXI@users.noreply.github.com>
2025-05-05 10:45:22 +0200

    chore: update package.json and package-lock.json to include wrangler dependency refactor: change next-sitemap.js to use ES module syntax

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,6 +1,6 @@
+
 /** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: 'https://rematch-cookbook.vercel.app',
-    generateRobotsTxt: true,
-  };
-  
+export default {
+  siteUrl: 'https://rematch-cookbook.vercel.app',
+  generateRobotsTxt: true,
+};
